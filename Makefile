6334736c5b8d229bc5b42703b150b5b0279e257d
Sam McSweeney <sammy.mcsweeney@gmail.com>
2017-07-20 16:34:36 +0800

    Now reads planetary ephemeris using CSPICE

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 INSTALL_DIR = /usr/local/bin
+CSPICE_DIR = $(HOME)/src/cspice/cspice
 
 veff: veff.c
-	gcc -Wall -Wextra -o $@ $< -lpsrcat -lm
+	gcc -Wall -Wextra -fsanitize=address -o $@ $< -I$(CSPICE_DIR)/include -L$(CSPICE_DIR)/lib -lasan -lpsrcat -lcspice -lm
+
+test: veff
+	./$< -p J0437-4715 -e 56559.878 -v -s /usr/local/share/jpl/de430.bsp
 
 install:
 	cp veff $(INSTALL_DIR)
--- veff.c
+++ veff.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <math.h>
 #include "psrcat.h"
+#include "SpiceUsr.h"
 
 #define PI          (4*atan(1.0))
 #define DEG2RAD(x)  ((x)*PI/180.0)
@@ -23,8 +24,22 @@ void usage()
     printf("    -e MJD     MJD epoch to calculate [required]\n");
     printf("    -h         Display this help and exit\n");
     printf("    -p PULSAR  Select pulsar PULSAR from psrcat catalogue [required]\n");
+    printf("    -s SPK     SPK = NASA planetary ephemeris file [required] (e.g.\n");
+    printf("               http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp)\n");
     printf("    -v         Turn verbose on (=show working)\n");
     printf("\n");
+}
+
+void normalise( vec *vin, vec *vout )
+{
+    // Normalise the vector length
+    double mag = sqrt(vin->x * vin->x +
+                      vin->y * vin->y +
+                      vin->z * vin->z);
+
+    vout->x = vin->x / mag;
+    vout->y = vin->y / mag;
+    vout->z = vin->z / mag;
 }
 
 double get_psrcat_value( char *psr, char *param )
@@ -65,9 +80,10 @@ void calc_vearth(vec *vearth)
 int main( int argc, char *argv[] )
 {
 
-    char *psr = NULL;
-    double epoch = 0.0;
-    int verbose = 0;
+    char   *psr       = NULL;
+    char   *ephemfile = NULL;
+    double  epoch     = 0.0;
+    int     verbose   = 0;
 
     // Parse command line options
     if (argc <= 1)
@@ -77,7 +93,7 @@ int main( int argc, char *argv[] )
     }
 
     int co;
-    while ((co = getopt(argc, argv, "e:hp:v")) != -1)
+    while ((co = getopt(argc, argv, "e:hp:s:v")) != -1)
     {
         switch (co)
         {
@@ -90,6 +106,9 @@ int main( int argc, char *argv[] )
                 break;
             case 'p':
                 psr = strdup(optarg);
+                break;
+            case 's':
+                ephemfile = strdup(optarg);
                 break;
             case 'v':
                 verbose = 1;
@@ -116,6 +135,45 @@ int main( int argc, char *argv[] )
         exit(EXIT_FAILURE);
     }
 
+    if (ephemfile == NULL)
+    {
+        fprintf(stderr, "error: option -s is required\n");
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    if (verbose)
+    {
+        printf( "Inputs:\n" );
+        printf( "  Ephemeris = %s\n",  ephemfile );
+        printf( "  Epoch     = %lf\n", epoch );
+        printf( "  Pulsar    = %s\n",  psr );
+    }
+
+    // Open ephemeris file
+    double jd = epoch + 2400000.5;
+    SpiceDouble et = (jd - j2000_c() ) * spd_c();
+    SpiceDouble state[6];
+    SpiceDouble lt;
+    furnsh_c( ephemfile );
+    spkezr_c( "earth", et, "j2000", "NONE", "solar system barycenter", state, &lt );
+    unload_c( ephemfile );
+
+    // Normalise the velocity vector
+    vec v_earth, vn_earth;
+    v_earth.x = state[3];
+    v_earth.y = state[4];
+    v_earth.z = state[5];
+    normalise( &v_earth, &vn_earth );
+
+    // Print out position and velocity of Earth
+    if (verbose)
+    {
+        printf("\nEarth pos:\n  [%f, %f, %f]\n", state[0], state[1], state[2]);
+        printf("Earth vel:\n  [%f, %f, %f]\n", v_earth.x, v_earth.y, v_earth.z );
+        printf("Earth vel (normalised):\n  [%f, %f, %f]\n", vn_earth.x, vn_earth.y, vn_earth.z );
+    }
+
     // Collect the needed values from psrcat
     double sini  = get_psrcat_value( psr, "sini"   );
     double ecc   = get_psrcat_value( psr, "ecc"    );
@@ -126,6 +184,8 @@ int main( int argc, char *argv[] )
     double dist  = get_psrcat_value( psr, "dist_a" );
     double pmra  = get_psrcat_value( psr, "pmra"   );
     double pmdec = get_psrcat_value( psr, "pmdec"  );
+    double rajd  = get_psrcat_value( psr, "rajd"   );
+    double decjd = get_psrcat_value( psr, "decjd"  );
 
     if (verbose) {
         printf("\nValues from PSRCAT:\n");
@@ -138,11 +198,15 @@ int main( int argc, char *argv[] )
         printf("  dist  = %.12f kpc\n", dist);
         printf("  pmra  = %.10f mas/yr\n", pmra);
         printf("  pmdec = %.10f mas/yr\n", pmdec);
+        printf("  rajd  = %.10f deg\n", rajd);
+        printf("  decjd = %.10f deg\n", decjd);
     }
 
     // Derive other values
-    double cosi = sqrt(1-sini*sini);
-    double komr = DEG2RAD(kom);
+    double cosi  = sqrt(1-sini*sini);
+    double komr  = DEG2RAD(kom);
+    double rajr  = DEG2RAD(rajd);
+    double decjr = DEG2RAD(decjd);
 
     // Other constants
     double c = 2.99792458e8;
@@ -152,7 +216,12 @@ int main( int argc, char *argv[] )
         printf("  c     = %.4f m/s\n", c);
         printf("  cosi  = %.12f\n", cosi);
         printf("  komr  = %.12f rad\n", komr);
+        printf("  rajr  = %.12f rad\n", rajr);
+        printf("  decjr = %.12f rad\n", decjr);
     }
+
+    free( psr );
+    free( ephemfile );
 
     return 0;
 }
